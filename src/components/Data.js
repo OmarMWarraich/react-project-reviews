dba3e91e308a217b88f2f8388b785df96818f770
OmarMWarraich <earthling986@gmail.com>
2021-04-29 05:02:18 +0500

    review

---
+++ src/components/Data.js
@@ -0,0 +1,58 @@
+const Reviews = [
+    {
+        id: 1,
+        name: 'Andrew Symonds',
+        job: 'web developer',
+        image: 'https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem nobis veritatis consequatur aut ducimus cupiditate, enim dolorem architecto beatae facilis maiores. Possimus voluptate, rerum quidem voluptatem quos deleniti quisquam nesciunt. Quibusdam optio veniam ipsum necessitatibus eos neque, dolor molestias cum adipisci dolorum enim quis beatae cupiditate quam assumenda ullam id commodi? Placeat voluptas ratione vel magni, laboriosam dicta officia accusamus! Quibusdam ipsam vero esse ipsa sapiente doloremque aut quam temporibus aperiam, nam molestias nisi, officia tempore ea cumque blanditiis vitae voluptatem repellendus praesentium eius voluptates, animi in. Perferendis, blanditiis laborum? Maiores sequi quisquam sapiente nam asperiores rerum dolorum tempora optio quidem laborum quos beatae ipsum deleniti natus est officiis, autem officia consequatur, suscipit sit necessitatibus! Animi sunt illum similique dolorum."
+        
+    },
+    {
+        id: 2,
+        name: 'Milla Jovovich',
+        job: 'web designer',
+        image: 'https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem nobis veritatis consequatur aut ducimus cupiditate, enim dolorem architecto beatae facilis maiores. Possimus voluptate, rerum quidem voluptatem quos deleniti quisquam nesciunt. Quibusdam optio veniam ipsum necessitatibus eos neque, dolor molestias cum adipisci dolorum enim quis beatae cupiditate quam assumenda ullam id commodi? Placeat voluptas ratione vel magni, laboriosam dicta officia accusamus! Quibusdam ipsam vero esse ipsa sapiente doloremque aut quam temporibus aperiam, nam molestias nisi, officia tempore ea cumque blanditiis vitae voluptatem repellendus praesentium eius voluptates, animi in. Perferendis, blanditiis laborum? Maiores sequi quisquam sapiente nam asperiores rerum dolorum tempora optio quidem laborum quos beatae ipsum deleniti natus est officiis, autem officia consequatur, suscipit sit necessitatibus! Animi sunt illum similique dolorum."
+        
+    },
+    {
+        id: 3,
+        name: 'Sikandar Khan',
+        job: 'Backend developer',
+        image: 'https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem nobis veritatis consequatur aut ducimus cupiditate, enim dolorem architecto beatae facilis maiores. Possimus voluptate, rerum quidem voluptatem quos deleniti quisquam nesciunt. Quibusdam optio veniam ipsum necessitatibus eos neque, dolor molestias cum adipisci dolorum enim quis beatae cupiditate quam assumenda ullam id commodi? Placeat voluptas ratione vel magni, laboriosam dicta officia accusamus! Quibusdam ipsam vero esse ipsa sapiente doloremque aut quam temporibus aperiam, nam molestias nisi, officia tempore ea cumque blanditiis vitae voluptatem repellendus praesentium eius voluptates, animi in. Perferendis, blanditiis laborum? Maiores sequi quisquam sapiente nam asperiores rerum dolorum tempora optio quidem laborum quos beatae ipsum deleniti natus est officiis, autem officia consequatur, suscipit sit necessitatibus! Animi sunt illum similique dolorum."
+        
+    },
+    {
+        id: 4,
+        name: 'Marc Anthony',
+        job: 'Data Scientist',
+        image: 'https://images.unsplash.com/photo-1500048993953-d23a436266cf?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem nobis veritatis consequatur aut ducimus cupiditate, enim dolorem architecto beatae facilis maiores. Possimus voluptate, rerum quidem voluptatem quos deleniti quisquam nesciunt. Quibusdam optio veniam ipsum necessitatibus eos neque, dolor molestias cum adipisci dolorum enim quis beatae cupiditate quam assumenda ullam id commodi? Placeat voluptas ratione vel magni, laboriosam dicta officia accusamus! Quibusdam ipsam vero esse ipsa sapiente doloremque aut quam temporibus aperiam, nam molestias nisi, officia tempore ea cumque blanditiis vitae voluptatem repellendus praesentium eius voluptates, animi in. Perferendis, blanditiis laborum? Maiores sequi quisquam sapiente nam asperiores rerum dolorum tempora optio quidem laborum quos beatae ipsum deleniti natus est officiis, autem officia consequatur, suscipit sit necessitatibus! Animi sunt illum similique dolorum."
+        
+    },
+    {
+        id: 5,
+        name: 'Stephany Johanes',
+        job: 'intern',
+        image: 'https://images.unsplash.com/photo-1573140247632-f8fd74997d5c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem nobis veritatis consequatur aut ducimus cupiditate, enim dolorem architecto beatae facilis maiores. Possimus voluptate, rerum quidem voluptatem quos deleniti quisquam nesciunt. Quibusdam optio veniam ipsum necessitatibus eos neque, dolor molestias cum adipisci dolorum enim quis beatae cupiditate quam assumenda ullam id commodi? Placeat voluptas ratione vel magni, laboriosam dicta officia accusamus! Quibusdam ipsam vero esse ipsa sapiente doloremque aut quam temporibus aperiam, nam molestias nisi, officia tempore ea cumque blanditiis vitae voluptatem repellendus praesentium eius voluptates, animi in. Perferendis, blanditiis laborum? Maiores sequi quisquam sapiente nam asperiores rerum dolorum tempora optio quidem laborum quos beatae ipsum deleniti natus est officiis, autem officia consequatur, suscipit sit necessitatibus! Animi sunt illum similique dolorum."
+        
+    },
+    {
+        id: 6,
+        name: 'Elizabeth Shue',
+        job: 'AI Specialist',
+        image: 'https://images.unsplash.com/photo-1578489758854-f134a358f08b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem nobis veritatis consequatur aut ducimus cupiditate, enim dolorem architecto beatae facilis maiores. Possimus voluptate, rerum quidem voluptatem quos deleniti quisquam nesciunt. Quibusdam optio veniam ipsum necessitatibus eos neque, dolor molestias cum adipisci dolorum enim quis beatae cupiditate quam assumenda ullam id commodi? Placeat voluptas ratione vel magni, laboriosam dicta officia accusamus! Quibusdam ipsam vero esse ipsa sapiente doloremque aut quam temporibus aperiam, nam molestias nisi, officia tempore ea cumque blanditiis vitae voluptatem repellendus praesentium eius voluptates, animi in. Perferendis, blanditiis laborum? Maiores sequi quisquam sapiente nam asperiores rerum dolorum tempora optio quidem laborum quos beatae ipsum deleniti natus est officiis, autem officia consequatur, suscipit sit necessitatibus! Animi sunt illum similique dolorum."
+        
+    }, {
+        id: 7,
+        name: 'peter aandre',
+        job: 'the boss',
+        image: 'https://images.unsplash.com/photo-1593219191991-47517a4bb8d5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem nobis veritatis consequatur aut ducimus cupiditate, enim dolorem architecto beatae facilis maiores. Possimus voluptate, rerum quidem voluptatem quos deleniti quisquam nesciunt. Quibusdam optio veniam ipsum necessitatibus eos neque, dolor molestias cum adipisci dolorum enim quis beatae cupiditate quam assumenda ullam id commodi? Placeat voluptas ratione vel magni, laboriosam dicta officia accusamus! Quibusdam ipsam vero esse ipsa sapiente doloremque aut quam temporibus aperiam, nam molestias nisi, officia tempore ea cumque blanditiis vitae voluptatem repellendus praesentium eius voluptates, animi in. Perferendis, blanditiis laborum? Maiores sequi quisquam sapiente nam asperiores rerum dolorum tempora optio quidem laborum quos beatae ipsum deleniti natus est officiis, autem officia consequatur, suscipit sit necessitatibus! Animi sunt illum similique dolorum."
+        
+    },
+]
+export default Reviews;
